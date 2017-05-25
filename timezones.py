bff8adc6656791c630eda82fbe7da11be12b4496
danagilliann <danalee96@gmail.com>
2017-05-25 12:31:41 +0200

    support for est to others. need to add unittests

--- timezones.py
+++ timezones.py
@@ -1,29 +1,69 @@
-def times(time): # => [est, pst]
+def from_est(time):
     time_arr = []
-    est = 6
-    pst = 9
+    cet = 5
+    pst = 3
+    gmt = 4
+
+    hour = int(time[0:2])
+    cet_hour = hour + cet
+    pst_hour = hour - pst
+    gmt_hour = hour + gmt
+
+    if cet_hour > 24:
+        cet_hour -= 24
+        cet_hour = "0" + str(cet_hour)
+    if pst_hour < 0:
+        pst_hour += 24
+    if gmt_hour > 24:
+        gmt_hour -= 24
+        gmt_hour = "0" + str(gmt_hour)
+
+    time_arr.append("CET: " + str(cet_hour) + ":" + time[3:5])
+    time_arr.append("PST: " + str(pst_hour) + ":" + time[3:5])
+    time_arr.append("GMT: " + str(gmt_hour) + ":" + time[3:5])
+
+    return time_arr
+
+def from_cet(time): # => [est, pst]
+    time_arr = []
+    est = 5
+    pst = 8
+    gmt = 1
 
     hour = int(time[0:2])
     est_hour = hour - est
     pst_hour = hour - pst
+    gmt_hour = hour - gmt
 
     if est_hour < 0:
         est_hour += 24
     if pst_hour < 0:
         pst_hour += 24
+    if gmt_hour < 0:
+        gmt_hour += 24
 
     time_arr.append("EST: " + str(est_hour) + ":" + time[3:5])
     time_arr.append("PST: " + str(pst_hour) + ":" + time[3:5])
+    time_arr.append("GMT: " + str(gmt_hour) + ":" + time[3:5])
 
     return time_arr
 
 def timezones():
     time = raw_input("enter time (eg: 3:00, 14:00): ")
+    zone = raw_input("enter timezone (GMT, EST, PST, or CET only): ")
+    zone.lower()
 
     if len(time) < 5:
         time = "0" + time
 
-    time = times(time)
+    if zone == "est":
+        time = from_est(time)
+    # elif zone == "gmt":
+    #     time = from_gmt(time)
+    # elif zone == "pst":
+    #     time = from_pst(time)
+    elif zone == "cet":
+        time = from_cet(time)
 
     for t in time:
         print t
